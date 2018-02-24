6bea6e8ca345fe42b451cb9e716c23f8b2a70dce
Eduardo Maíllo Conesa <eduardomaillo@gmail.com>
2018-02-24 11:42:24 -0500

    componentdidmount and verifyuser functionality

--- server/sessionController.js
+++ server/sessionController.js
@@ -5,15 +5,20 @@ const sessionController = {};
 sessionController.verifySession = (req, res) => {
   Session.findOne({cookieId: req.cookies.ssid}, (err, result) => {
     if(err || !result){
-      return res.redirect('/signup');
+      return res.send({
+        status: false
+      });
     }
   //res.render(main page)
   });
 };
 
 sessionController.startSession = (req, res) => {
+  let session = new Session ({
+    cookieId: req.cookies.ssid
+  });
   session.save(function(error, session) {
-    if(error) return res.render('/signup', {error: error});
+    if(error) return res.send(true);
     //res.render(main page)
   });
 };
